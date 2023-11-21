2efe836b0ea48da844514d07775d7ea2a3aa5cfa
Abby A <abidextrous@gmail.com>
2023-11-21 09:16:08 +0000

    created model entity and view finder files. queried database and displayed all seeds onto page. created .gitignore file in prep for testing composer

---
+++ src/SeedsViewer.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once 'src/Seed.php';
+require_once 'src/SeedsModel.php';
+
+class SeedsViewer
+{
+    // public static function displaySingleSeed(Seed $seed): string
+    // {
+    //         $output = '<div>';
+    //         $output .= "<h1>$seed->name</h1>";
+    //         $output .= "<p>$seed->family_name</p>";
+    //         $output .= "<p>$seed->species</p>";
+    //         $output .= "<p>$seed->description</p>";
+    //         $output .= "<img src='$seed->image' />";
+    //         $output .= '</div>';
+
+    //     return $output;
+    // }
+
+    public static function displayAllSeeds(array $seeds): string
+    {
+        $output = '';
+
+        foreach ($seeds as $seed) {
+            $output .= '<div>';
+            $output .= "<h1>$seed->name</h1>";
+            $output .= "<p>$seed->family_name</p>";
+            $output .= "<p>$seed->species</p>";
+            $output .= "<p>$seed->description</p>";
+            $output .= "<img src='$seed->image' />";
+            $output .= '</div>';
+        }
+
+        return $output;
+    }
+}
